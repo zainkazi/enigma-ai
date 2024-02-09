97d1e47aa588ff522cd54f7c97c598cf580c2589
Zain <zainkazi27@gmail.com>
2024-02-09 20:17:11 +0500

    added body to avatar API

--- app/api/avatar/route.ts
+++ app/api/avatar/route.ts
@@ -1,12 +1,19 @@
 import openai from "@/utils/openai";
 import { NextRequest, NextResponse } from "next/server";
 
-const imagePrompt = `Generate a realistic portrait of a [ethnicity] [age group] [gender] with [hair color], facing directly towards the viewer as if they are a news reporter speaking to the camera. Ensure that the background is neutral and unobtrusive, focusing solely on the subject. Exclude any additional elements or objects from the image.`;
-
 export const POST = async (request: NextRequest) => {
+  const { ethnicity, ageGroup, gender, hairColor, numberOfCharacters } =
+    await request.json();
+
+  const imagePrompt = `Generate a realistic portrait of a ${ethnicity} ${ageGroup} ${gender} with ${hairColor} hair color, facing directly towards the viewer as if they are a news reporter speaking to the camera. Ensure that the background is neutral and unobtrusive, focusing solely on the subject. Exclude any additional elements or objects from the image.`;
+
+  console.log(ethnicity, ageGroup, gender, hairColor, numberOfCharacters);
+  console.log(typeof numberOfCharacters);
+
   const image = await openai.images.generate({
     model: "dall-e-2",
-    prompt: "cute puppy",
+    prompt: imagePrompt,
+    n: numberOfCharacters,
   });
 
   return NextResponse.json(image);
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <main>Hello</main>;
+  return <main className="text-red">Hello</main>;
 }
