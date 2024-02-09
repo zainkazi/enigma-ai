87ee278c2f65e575a043a909a48df3d7f96af17f
Zain <zainkazi27@gmail.com>
2024-02-09 20:22:44 +0500

    added body to speech API

--- app/api/speech/route.ts
+++ app/api/speech/route.ts
@@ -2,9 +2,8 @@ import openai from "@/utils/openai";
 import supabase from "@/utils/supabase";
 import { NextRequest, NextResponse } from "next/server";
 
-const speechInput = "Today is a wonderful day to build something people love!";
-
 export const POST = async (request: NextRequest) => {
+  const { speechInput }: { speechInput: string } = await request.json();
   const mp3 = await openai.audio.speech.create({
     model: "tts-1",
     voice: "alloy",
@@ -14,10 +13,7 @@ export const POST = async (request: NextRequest) => {
   const speech = Buffer.from(await mp3.arrayBuffer());
   const { data, error } = await supabase.storage
     .from("speeches")
-    .upload("public/speech1.mp3", speech, {
-      cacheControl: "3600",
-      upsert: false,
-    });
+    .upload("public/speech2.mp3", speech);
 
   if (error) {
     return NextResponse.json({ error }, { status: 400 });
