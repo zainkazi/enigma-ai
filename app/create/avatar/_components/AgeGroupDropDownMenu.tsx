ef1fb0c255f3c72c8425a5c4cf6df54830a9719c
Zain <zainkazi27@gmail.com>
2024-02-17 22:02:48 +0500

    updated the age selector to store state

--- app/create/avatar/_components/AgeGroupDropDownMenu.tsx
+++ app/create/avatar/_components/AgeGroupDropDownMenu.tsx
@@ -1,11 +1,18 @@
 "use client";
 
-import React, { useState, useRef, useEffect } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useAvatarStore } from "@/store";
 
 const AgeGroupDropDownMenu = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [selectedAgeGroup, setSelectedAgeGroup] = useState("Select Age Group");
-  const dropdownRef = useRef(null);
+  const setAgeGroup = useAvatarStore((state) => state.setFormData);
 
   const ageGroups = [
     "0 - 12",
@@ -18,46 +25,24 @@ const AgeGroupDropDownMenu = () => {
     "65+",
   ];
 
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-        setIsOpen(false);
-      }
-    };
-
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [dropdownRef]);
-
   return (
-    <div className="relative" ref={dropdownRef}>
-      <h1 className=" text-lg font-medium py-2">Age Group</h1>
-      <button
-        className="border border-zinc-100 w-64 text-white px-4 py-2 rounded-md flex items-center justify-between"
-        onClick={() => setIsOpen(!isOpen)}
-      >
-        {selectedAgeGroup} <span className="ml-2">▼</span>
-      </button>
-      {isOpen && (
-        <div className="absolute mt-1 w-full rounded-md shadow-lg bg-zinc-900 z-50">
-          <ul className="py-1">
-            {ageGroups.map((ageGroup, index) => (
-              <li
-                key={index}
-                className="px-4 py-2 hover:bg-zinc-800 cursor-pointer text-white"
-                onClick={() => {
-                  setSelectedAgeGroup(ageGroup);
-                  setIsOpen(false);
-                }}
-              >
+    <div>
+      <h1 className="text-lg font-medium py-2">Age Group</h1>
+      <Select onValueChange={(value) => setAgeGroup("ageGroup", value)}>
+        <SelectTrigger>
+          <SelectValue placeholder="Select age group" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectLabel>Age Groups</SelectLabel>
+            {ageGroups.map((ageGroup) => (
+              <SelectItem key={ageGroup} value={ageGroup}>
                 {ageGroup}
-              </li>
+              </SelectItem>
             ))}
-          </ul>
-        </div>
-      )}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
     </div>
   );
 };
