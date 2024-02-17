0b91e18dafa479d0c8023573a51e4524c65696d9
Zain <zainkazi27@gmail.com>
2024-02-17 22:31:53 +0500

    updated character quantity selector to store state

--- app/create/avatar/_components/CharacterQuantitySelector.tsx
+++ app/create/avatar/_components/CharacterQuantitySelector.tsx
@@ -1,9 +1,13 @@
 "use client";
 
+import { useAvatarStore } from "@/store";
 import React, { useState } from "react";
 
 const CharacterQuantitySelector = () => {
-  const [selectedQuantity, setSelectedQuantity] = useState(1);
+  const numberOfCharacters = useAvatarStore(
+    (state) => state.formData.numberOfCharacters
+  );
+  const setNumberOfCharacters = useAvatarStore((state) => state.setFormData);
   const quantities = [1, 2, 3, 4];
 
   return (
@@ -14,11 +18,13 @@ const CharacterQuantitySelector = () => {
           <button
             key={quantity}
             className={`w-10 h-10 rounded-full mx-1 text-center ${
-              selectedQuantity === quantity
+              numberOfCharacters === quantity
                 ? " border-indigo-600 bg-indigo-600 border-2 "
                 : "border-2 border-zinc-100 "
             }`}
-            onClick={() => setSelectedQuantity(quantity)}
+            onClick={() =>
+              setNumberOfCharacters("numberOfCharacters", quantity)
+            }
           >
             {quantity}
           </button>
