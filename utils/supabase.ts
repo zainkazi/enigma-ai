4217725943aee85375bcdb67effc214c507df2d1
Zain <zainkazi27@gmail.com>
2024-02-23 21:02:07 +0500

    moved the environment variables in .env.local

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-  "https://abhmleehngupqjxbgsez.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFiaG1sZWVobmd1cHFqeGJnc2V6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDYxOTMzNjYsImV4cCI6MjAyMTc2OTM2Nn0.HdBwhCEfdx4sMhysMRO2q9UGrOH8MEZR7IUWkK1bBEQ"
+  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
 );
 
 export default supabase;
