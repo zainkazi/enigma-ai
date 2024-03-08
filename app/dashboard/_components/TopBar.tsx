775a8094cad13fa6885e92b01ccf4a6b1f42e0d9
zain <zainkazi27@gmail.com>
2024-03-09 00:16:25 +0500

    added theme toggle

--- app/dashboard/_components/TopBar.tsx
+++ app/dashboard/_components/TopBar.tsx
@@ -1,25 +1,19 @@
-"use client";
-
 import { Card } from "@/components/ui/card";
 import { UserButton } from "@clerk/nextjs";
 import { Coins } from "lucide-react";
-import React, { Suspense, useState } from "react";
-
+import ThemeToggle from "@/app/ThemeToggle";
 function TopBar() {
-  const [userTokens, setUserTokens] = useState(3000);
+  const userTokens = 3000;
 
   return (
     <section className="flex justify-end">
-      <div className="flex space-x-4">
+      <div className="flex space-x-4 items-center">
+        <ThemeToggle />
         <Card className="flex items-center border-2 justify-center py-2 px-4 space-x-2 rounded-full">
           <Coins className="text-yellow-300" />
           <h1>{userTokens}</h1>
         </Card>
-        <div className="flex items-center justify-center space-x-2">
-          <Suspense fallback={<div>Loading...</div>}>
-            <UserButton />
-          </Suspense>
-        </div>
+        <UserButton />
       </div>
     </section>
   );
