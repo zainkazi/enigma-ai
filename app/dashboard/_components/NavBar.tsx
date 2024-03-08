9ac53dbb7606a2ea0a377914e0edb787d7ff1e6c
zain <zainkazi27@gmail.com>
2024-03-09 00:27:39 +0500

    added settings button

--- app/dashboard/_components/NavBar.tsx
+++ app/dashboard/_components/NavBar.tsx
@@ -1,13 +1,23 @@
+import { Button } from "@/components/ui/button";
 import CreateProjectDialog from "./CreateProjectDialog";
+import { Settings } from "lucide-react";
+import Link from "next/link";
 
 function NavBar() {
   return (
-    <section className="flex justify-center w-full">
+    <section className="flex flex-col justify-between h-full w-full">
       <div className="space-y-4 w-full">
         <h1 className="text-center text-4xl">ENIGMA AI</h1>
         <CreateProjectDialog />
       </div>
-      <nav></nav>
+      <div>
+        <Link href="/settings">
+          <Button variant="ghost">
+            <Settings className="mr-2 w-5" />
+            Settings
+          </Button>
+        </Link>
+      </div>
     </section>
   );
 }
