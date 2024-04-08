d291d5846280042459995b754359c1d98c8a134f
zain <zainkazi27@gmail.com>
2024-04-09 02:07:04 +0500

    minor changes

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,9 @@
-import NavBar from "./_components/NavBar";
 import Projects from "./_components/Projects";
 import TopBar from "./_components/TopBar";
 import { currentUser } from "@clerk/nextjs";
 import prisma from "@/utils/db";
-import { unstable_noStore as noStore } from "next/cache";
-
-export const dynamic = "force-dynamic";
 
 async function DashboardPage() {
-  noStore();
   const user = await currentUser();
   const match = await prisma.user.findUnique({
     where: {
